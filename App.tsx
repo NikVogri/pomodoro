fe913ed22bf06071e89b3b8702335ab8e35c41ef
NikVogri <vogrinec.nik@gmail.com>
2022-07-10 23:37:55 +0200

    feat: ask for notification permissions on app startup

--- App.tsx
+++ App.tsx
@@ -1,8 +1,36 @@
 import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import { setNotificationHandler } from "expo-notifications";
+import { notificationsPermissionHandler } from "./services/notification/NotificationsPermissionHandler";
+import { localNotificationsScheduler } from "./services/notification/LocalNotificationsScheduler";
 
 import Navigation from "./Navigation";
 
+// TODO: Add own notification handler here.
+setNotificationHandler({
+	handleNotification: async () => ({
+		shouldShowAlert: true,
+		shouldPlaySound: false,
+		shouldSetBadge: false,
+	}),
+});
+
 export default function App() {
+	useEffect(() => {
+		const bootstrap = async () => {
+			const { hasPermission, canAskAgain } = await notificationsPermissionHandler.getPermissionsStatus();
+
+			if (!hasPermission && canAskAgain) {
+				await notificationsPermissionHandler.requestPermissions();
+			}
+		};
+
+		bootstrap();
+
+		return () => {
+			localNotificationsScheduler.cancelAllScheduledNotifications();
+		};
+	}, []);
 	return (
 		<>
 			<StatusBar style="auto" />
