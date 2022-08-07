f0868090f0e389c9557f4776a58684e7c719cb57
NikVogri <vogrinec.nik@gmail.com>
2022-08-07 21:37:14 +0200

    feat: local storage service & dependency installation

---
+++ services/local-storage/LocalStorage.ts
@@ -0,0 +1,27 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+import { generateUuid } from "../../util/generateUuid";
+
+export class LocalStorage {
+	async storeData(data: string, key?: string): Promise<void> {
+		const itemId = key ? key : generateUuid();
+		await AsyncStorage.setItem(itemId, data);
+	}
+
+	async getRecord<T>(key: string): Promise<T | void> {
+		const data = await AsyncStorage.getItem(key);
+		if (!data) return undefined;
+
+		return JSON.parse(data) as T;
+	}
+
+	async getAllKeys(): Promise<string[]> {
+		return (await AsyncStorage.getAllKeys()) as string[];
+	}
+
+	async getMultipleRecords(keys: string[]) {
+		return await AsyncStorage.multiGet(keys);
+	}
+}
+
+export const localStorage = new LocalStorage();
